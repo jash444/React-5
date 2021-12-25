a45f43d0b87848c6d3529d97a7adeb02b5421f0f
jaswanth <geddamjaswanth4@gmail.com>
2021-12-25 12:35:50 +0530

    react

---
+++ react-1/public/compounds/todo.js
@@ -0,0 +1,4 @@
+function Todo ({num}){
+    return <h1>todo:{num}</h1>
+}
+export default Todo;
--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [age,setAge]=useState(0)
+
+  function addOne(a){
+    setAge( age+a )
+  }
+  
+if(age>10){
+  return <div>coutre reach max value</div>
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="App">
+     <h3>counter:{age}</h3>
+
+          <button onClick={()=>addOne(1)}>add 1</button>
+        <button onClick={()=>addOne(-1)}>reduce 1</button>
+      
+
     </div>
   );
 }
